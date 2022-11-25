75ceab0feb4dc9988cbe9f73761b5304a3dabe24
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-11-25 14:28:08 +0500

    creted todolist

---
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -0,0 +1,7 @@
+import styles from "./ProjectsList.module.css";
+
+const ProjectsList = () => {
+  return <div className={styles.list}></div>;
+};
+
+export default ProjectsList;
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,7 @@
+import styles from "./TodoList.module.css";
+
+const TodoList = () => {
+  return <div className={styles.list}></div>;
+};
+
+export default TodoList;
