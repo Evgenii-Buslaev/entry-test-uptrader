4203a08babf0653db67e5517c93f9d357b579fc4
Evgenii Buslaev <eugenebus144@gmail.com>
2022-11-26 22:16:39 +0500

    added create todo btn

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,6 +2,9 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import TodoItem from "../TodoItem/TodoItem";
+import Button from "../../UI/Button/Button";
+
+import create from "../../assets/icons/create.png";
 import styles from "./TodoList.module.css";
 
 const TodoList = () => {
@@ -18,26 +21,29 @@ const TodoList = () => {
   const { queue, development, done } = todos;
 
   return (
-    <div className={styles.list}>
-      <section className={styles.column}>
-        <h3>Queue</h3>
-        {queue.map((todo) => (
-          <TodoItem key={Math.random()} title={todo.title} />
-        ))}
-      </section>
-      <section className={styles.column}>
-        <h3>Development</h3>
-        {development.map((todo) => (
-          <TodoItem key={Math.random()} title={todo.title} />
-        ))}
-      </section>
-      <section className={styles.column}>
-        <h3>Done</h3>
-        {done.map((todo) => (
-          <TodoItem key={Math.random()} title={todo.title} done={todo.done} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Button path={create} alt="create todo" />
+      <div className={styles.list}>
+        <section className={styles.column}>
+          <h3>Queue</h3>
+          {queue.map((todo) => (
+            <TodoItem key={Math.random()} title={todo.title} />
+          ))}
+        </section>
+        <section className={styles.column}>
+          <h3>Development</h3>
+          {development.map((todo) => (
+            <TodoItem key={Math.random()} title={todo.title} />
+          ))}
+        </section>
+        <section className={styles.column}>
+          <h3>Done</h3>
+          {done.map((todo) => (
+            <TodoItem key={Math.random()} title={todo.title} done={todo.done} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 };
 
