600b9bf08176a176cb06f413b7327040b0d4680f
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-11-27 19:47:19 +0500

    set modal opening from todo list

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,14 +1,23 @@
-import { useSelector } from "react-redux";
+import { useState, useContext } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import TodoItem from "../TodoItem/TodoItem";
 import Button from "../../UI/Button/Button";
+import InputText from "../../UI/InputText/InputText";
+import Modal from "../../components/Modal/Modal";
+import Form from "../Form/Form";
+import { ModalContext } from "../../context/context";
 
 import create from "../../assets/icons/create.png";
 import styles from "./TodoList.module.css";
 
 const TodoList = () => {
   const params = useParams();
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const { state } = useContext(ModalContext);
+  const toggleModal = () => state.setModalOpenned(!state.modalOpenned);
 
   let todos;
   if (params.id) {
@@ -20,9 +29,25 @@ const TodoList = () => {
   }
   const { queue, development, done } = todos;
 
+  const formComponents = [
+    {
+      element: (
+        <InputText
+          value={todoTitle}
+          change={setTodoTitle}
+          text="Todo's title"
+          key="Todo's title"
+        />
+      ),
+    },
+  ];
+
+  const modalChildren = <Form list={formComponents} action="Create todo" />;
+
   return (
     <>
-      <Button path={create} alt="create todo" />
+      <Button path={create} alt="create todo" click={toggleModal} />
+      <Modal children={modalChildren} />
       <div className={styles.list}>
         <section className={styles.column}>
           <h3>Queue</h3>
